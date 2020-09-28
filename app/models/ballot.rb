641e2cd5afaec648a795fb1793526c7d7815c03b
ryosuke.mega <ryosuke.mega@gmail.com>
2020-09-28 12:35:13 +0000

    for heroku4

--- app/models/ballot.rb
+++ app/models/ballot.rb
@@ -1,6 +1,6 @@
 class Ballot < ApplicationRecord
   validates :candidate_id, uniqueness: { scope: :user_id }
-  validates :election_id, uniqueness: { scope: :user_id }
+  # validates :election_id, uniqueness: { scope: :user_id }
   belongs_to :user
   belongs_to :candidate
   belongs_to :election
